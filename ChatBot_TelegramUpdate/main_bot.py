18827dd25de0d263cc5d98c823d01bcb6aa28105
Messias Martins <35672212+MartinsMessias@users.noreply.github.com>
2018-09-10 01:42:12 -0500

    Add files via upload

---
+++ ChatBot_TelegramUpdate/main_bot.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+from telegram.ext import Updater, CommandHandler, RegexHandler
+import os
+
+ativ = True
+bot_ = ChatBot('Clovis')
+bot_.set_trainer(ListTrainer)
+
+for arqv in os.listdir('arqv'):
+    chats = open('arqv/' + arqv, 'r').readlines()
+    bot_.train(chats)
+
+
+def hello(bot, update):
+    resq = update['message']['text']
+    print('Response: ', resq)
+    update.message.reply_text(
+        '{}'.format(bot_.get_response(resq)))
+
+
+updater = Updater('695940313:AAGoX81El490A9lpokCYDSa2nbgF3JCHa4Y')
+
+updater.dispatcher.add_handler(RegexHandler('', hello))
+updater.start_polling()
+updater.idle()
